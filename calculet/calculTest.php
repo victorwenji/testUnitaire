6c9776182ce096005bffe194ab2b967bfa2eb32a
WENJI Victor Pascal <68334507+victorwenji@users.noreply.github.com>
2023-01-18 15:08:50 +0100

    sonar9
    sonar9

--- calculet/calculTest.php
+++ calculet/calculTest.php
@@ -8,47 +8,49 @@
         //verifie si laddition fonctionne 
         public function TestAdd()
         {
-            $Calc = new calcul();
-            $this->assertEquals(5, $Calc->add(2, 3));
+            $calc = new calcul();
+            $this->assertEquals(5, $calc->add(2, 3));
         }
+
         public function TestAddN1IsNull()
         {
-            $Calc = new calcul();
+            $calc = new calcul();
             $this->expectException(Exception::class);
             $this->expectExceptionMessage("N1 ne dois pas etre null!! remplacer N1!");
-            $Calc->add(null, 5);
+            $calc->add(null, 5);
             //$this->assertEquals(5, $calc->add(2,3));
         }
         public function TestAddN2IsNull()
         {
-            $Calc = new calcul();
+            $calc = new calcul();
             $this->expectException(Exception::class);
             $this->expectExceptionMessage("N2 ne dois pas etre null!! remplacer N2!");
-            $Calc->add(5, null);
+            $calc->add(5, null);
             //$this->assertEquals(5, $calc->add(2, 3));
         }
         public function TestSou()
         {
-            $Calc = new calcul();
-            $this->assertEquals(5, $Calc->sou(8, 3));
+            $calc = new calcul();
+            $this->assertEquals(5, $calc->sou(8, 3));
         }
         public function TestMul()
         {
-            $Calc = new calcul();
-            $this->assertEquals(6, $Calc->Mul(2, 3));
+            $calc = new calcul();
+            $this->assertEquals(6, $calc->Mul(2, 3));
         }
         public function TestDivN2IsNull()
         {
-            $Calc = new calcul();
+            $calc = new calcul();
+            
             $this->expectException(Exception::class);
             $this->expectExceptionMessage("impossible de diviser par 0 !");
-            $Calc->div(15, 0);
+            $calc->div(15, 0);
             // $this->assertEquals(5, $calc->div(15, 0));
         }
         public function testDiv()
         {
-            $Calc = new calcul();
-            $this->assertEquals(5, $Calc->div(15, 3));
+            $calc = new calcul();
+            $this->assertEquals(5, $calc->div(15, 3));
         }
     }
 ?>
