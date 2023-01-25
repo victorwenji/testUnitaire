53b6f36b3f6a0ade4279d5eab57065f0affd27be
WENJI Victor Pascal <68334507+victorwenji@users.noreply.github.com>
2023-01-25 09:43:24 +0100

    Update functionTest.php

--- contact/functionTest.php
+++ contact/functionTest.php
@@ -1,20 +1,17 @@
 <?php
-    include('function.php');
-    
+    use contact\function;
+
     use PHPUnit\Framework\TestCase;
     class functionTest extends TestCase
     {
         protected $pdo;
         public function getConnection()
         {
-            
         }
-
         public function testinsert()
         {
             $info = new information();
             $info->insert(5, "pascal", "loic");
-        }        
-}
+        }
+    }
 
-?>
