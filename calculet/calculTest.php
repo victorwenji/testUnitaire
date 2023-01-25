9acf4dafa9090918239deb7bc2ea917ccf7a0144
WENJI Victor Pascal <68334507+victorwenji@users.noreply.github.com>
2023-01-25 09:31:57 +0100

    Update calculTest.php

--- calculet/calculTest.php
+++ calculet/calculTest.php
@@ -1,24 +1,20 @@
 <?php
-    /*class de test*/
     use PHPUnit\Framework\TestCase;
     use calculet\calcul;
-    /*use calcul;*/
+
     class CalculTest extends TestCase
     {
-        /*verifie si laddition fonctionne */
         public function TestAdd()
         {
             $calc = new calcul();
             $this->assertEquals(5, $calc->add(2, 3));
         }
-
         public function TestAddN1IsNull()
         {
             $calc = new calcul();
             $this->expectException(Exception::class);
             $this->expectExceptionMessage("N1 ne dois pas etre null!! remplacer N1!");
             $calc->add(null, 5);
-            /*$this->assertEquals(5, $calc->add(2,3));*/
         }
         public function TestAddN2IsNull()
         {
@@ -26,7 +22,6 @@
             $this->expectException(Exception::class);
             $this->expectExceptionMessage("N2 ne dois pas etre null!! remplacer N2!");
             $calc->add(5, null);
-            /*$this->assertEquals(5, $calc->add(2, 3));*/
         }
         public function TestSou()
         {
@@ -41,11 +36,9 @@
         public function TestDivN2IsNull()
         {
             $calc = new calcul();
-            
             $this->expectException(Exception::class);
             $this->expectExceptionMessage("impossible de diviser par 0 !");
             $calc->div(15, 0);
-            /* $this->assertEquals(5, $calc->div(15, 0));*/
         }
         public function testDiv()
         {
