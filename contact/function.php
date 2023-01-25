c693bf466532db25cbcbd5b3171cd3de9ac1056b
WENJI Victor Pascal <68334507+victorwenji@users.noreply.github.com>
2023-01-25 09:35:55 +0100

    sonar

--- contact/function.php
+++ contact/function.php
@@ -1,36 +1,31 @@
 <?php
- //fonction de la table
-//  require 'DBcon.php';
-class information{
+class Information
+{
     private $pdo;
-
     function __construct() {
         $this->pdo  = new PDO('sqlite:C:\\Sqlite\\info');
     }
-     
-    function insert($id,$firstname,$lastname){  
+    function insert($id,$firstname,$lastname)
+    {
         $sql = "INSERT INTO contact(id, name, lastname) values(?, ?, ?)";
         $q = $this->pdo->prepare($sql);
-        $q->execute(array( $id, $firstname, $lastname));     
+        $q->execute(array( $id, $firstname, $lastname)); 
     }
-
-    function select($id){
+    function select($id)
+    {
         $pdo=Database::connect();
         $sql = "SELECT * FROM contact where id =?";
         $q = $pdo->prepare($sql);
         $q->execute(array($id));
         $data = $q->fetch(PDO::FETCH_ASSOC);
-
         Database::disconnect();
     }
-
     function update($id, $name, $lastname)
     {
         $pdo=Database::connect();
         $sql = "UPDATE contact SET id =$id, name = $name, lastname = $lastname";
         $q = $pdo->prepare($sql);
         $q->execute(array($id, $name, $firstname));
-
         Database::disconnect();
     }
     function delete($id)
@@ -39,14 +34,6 @@ class information{
         $sql = "DELETE FROM user  WHERE id = $id";
         $q = $pdo->prepare($sql);
         $q->execute(array($id));
-
         Database::disconnect();
     }
-
 }
-?>
-
-
-
-
-
