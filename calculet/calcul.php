7775b5853f29b88f341259101cf1171105ff51e8
WENJI Victor Pascal <68334507+victorwenji@users.noreply.github.com>
2023-01-25 09:13:03 +0100

    Update calcul.php

--- calculet/calcul.php
+++ calculet/calcul.php
@@ -3,7 +3,6 @@
     {
        const T = "N1 ne dois pas etre null!! remplacer N1!";
        const N = "N2 ne dois pas etre null!! remplacer N2!";
-       
         function add($nombre1, $nombre2)
         {
             if ($nombre1 == null) {
@@ -13,7 +12,7 @@
                 throw new InvalidArgumentException("".N);
             }
             $result = $nombre1 + $nombre2;
-            return $result;  
+            return $result;
         }
         function Sou($nombre1, $nombre2)
         {
@@ -38,16 +37,16 @@
             {
                 throw new InvalidArgumentException("".N);
             }
-            $result = $nombre1 * $nombre2; 
+            $result = $nombre1 * $nombre2;
             return $result;
         }
         function div($nombre1, $nombre2)
         {
             if ($nombre2==0)
             {
-                throw new InvalidArgumentException("impossible de diviser par 0 !"); 
+                throw new InvalidArgumentException("impossible de diviser par 0 !");
             }
-            $result = $nombre1 / $nombre2; 
+            $result = $nombre1 / $nombre2;
             return $result;
         }
     }
